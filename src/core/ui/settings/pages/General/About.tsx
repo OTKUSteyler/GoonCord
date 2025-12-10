b99e851af9d1f4118c2a8a72e75419d65271248e
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-12-10 16:10:31 +0100

    Update About.tsx

--- src/core/ui/settings/pages/General/About.tsx
+++ src/core/ui/settings/pages/General/About.tsx
@@ -97,7 +97,7 @@ export default function About() {
             variant="text-md/medium"
             style={{ textAlign: "center", marginTop: 4 }}
           >
-            Jolly your discord client!
+            Goon your discord client!
           </Text>
         </View>
 
