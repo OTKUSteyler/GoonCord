3a3d61971a153ea23673f62db7fa1ee37a1a066e
OTKUSteyler <qsanom@proton.me>
2026-04-02 17:12:51 +0200

    Update index.ts

--- src/core/plugins/index.ts
+++ src/core/plugins/index.ts
@@ -36,6 +36,7 @@ export const getCorePlugins = (): CorePluginRecord => ({
   "bunny.notrack": makeLazyPlugin(() => require("./notrack")),
   "bunny.messagefix": makeLazyPlugin(() => require("./messagefix")),
   "bunny.fixembed": makeLazyPlugin(() => require("./fixembed")),
+  "bunny.nodelete": makeLazyPlugin(() => require("./NoDelete")),
 });
 
 /**
